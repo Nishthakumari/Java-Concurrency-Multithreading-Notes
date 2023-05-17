7e1a1bbce8eaede952ddae1e15e1e23b7a1ed224
Nishthakumari <knishtha0@gmail.com>
2023-05-17 14:25:20 +0530

    Thread Creation using Runnable Interface

--- Code/multiThreadingClass/ThreadTester.java
+++ Code/multiThreadingClass/ThreadTester.java
@@ -6,8 +6,23 @@ public class ThreadTester {
 
         System.out.println("main is starting");
         Thread thread1 = new Thread1("thread1");
-        thread1.setDaemon(true);
+
         thread1.start();
+
+
+        Thread thread2 = new Thread(new Thread2(), "thread2");
+        thread2.start();
+
+        Thread thread3 = new Thread(()->{
+            for(int i=10;i<15;i++)
+            {
+                System.out.println(Thread.currentThread() + ", "+i);
+            }
+        }, "thread3");
+
+        thread3.start();
+
+
         System.out.println("main is ending");
     }
 }
