49ac9cd63ff2a87dc446474cad10a4bec193501d
Nishthakumari <knishtha0@gmail.com>
2023-05-19 16:32:32 +0530

    Using Objects as Locks

--- Code/multiThreadingClass/src/main/java/multiThreadingClass/Stack.java
+++ Code/multiThreadingClass/src/main/java/multiThreadingClass/Stack.java
@@ -4,35 +4,46 @@ public class Stack {
 
     private int[] array;
     private int stackTop;
+    Object lock;
 
     public Stack(int capacity)
     {
         array = new int[capacity];
         stackTop = -1;
+        lock = new Object();
     }
 
     public boolean push(int element){
-        if(isFull()) return false;
-        ++stackTop;
+        synchronized (lock) {
+            if (isFull()) return false;
+            ++stackTop;
 
-        try{Thread.sleep(1000);} catch (Exception e){}
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+            }
 
-        array[stackTop] = element;
+            array[stackTop] = element;
 
-        return true;
+            return true;
+        }
 
     }
 
     public int pop(){
-        if(isEmpty()) return Integer.MIN_VALUE;
-        int obj = array[stackTop];
-        array[stackTop] = Integer.MIN_VALUE;
+        synchronized (lock) {
+            if (isEmpty()) return Integer.MIN_VALUE;
+            int obj = array[stackTop];
+            array[stackTop] = Integer.MIN_VALUE;
 
-        try{Thread.sleep(1000);} catch (Exception e){}
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+            }
 
-        stackTop--;
-        return obj;
-
+            stackTop--;
+            return obj;
+        }
 
     }
 
